e5d56d3a06b4f1144a934d02f1a280fa1d6b98b1
chriscauley <chris@lablackey.com>
2022-04-08 19:22:27 -0400

    console.log

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import EventLog from './components/EventLog.vue'
 import '@unrest/tailwind/dist.css'
 import './css/index.css'
 
-console.log(1)
-
 createApp(App)
   .use(router)
   .use(unrest.plugin)
